114339d3c1844e0a9ee3b49966202ad4165b2c35
swetaduari <161187872+swetaduari@users.noreply.github.com>
2026-07-24 16:43:35 +0530

    Update AdminLayout.jsx

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,19 +1,12 @@
 import { Outlet } from "react-router-dom";
-import AdminSidebar from "../pages/admin/AdminSidebar";
 
 export default function AdminLayout() {
 
     return (
 
-        <div className="flex">
+        <div className="min-h-screen bg-gray-100">
 
-            <AdminSidebar />
-
-            <div className="flex-1 bg-gray-100 min-h-screen p-8">
-
-                <Outlet />
-
-            </div>
+            <Outlet />
 
         </div>
 
